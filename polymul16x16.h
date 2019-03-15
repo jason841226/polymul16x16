081606271ba431359deaba1fe54467f3935bc81e
jason841226 <b03901053@ntu.edu.tw>
2019-03-15 13:50:53 +0800

    Add files via upload

--- polymul16x16.h
+++ polymul16x16.h
@@ -1,16 +1,27 @@
+#ifndef POLYMUL16X16
+#define POLYMUL16X16
+
 #include <immintrin.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 
-
-//TO TRY: 256bit shift(alignr and srli)
-
-
 #define v4591_16 _mm256_set1_epi16(4591)
 #define v15631_16 _mm256_set1_epi16(15631)
 #define v4158_16 _mm256_set1_epi16(4158)
 #define v1_16 _mm256_set1_epi16(1)
+
+void print256_num(__m256i var);
+static inline void print_cmp(__m256i _x,__m256i _y);
+static inline __m256i montproduct(__m256i x,__m256i y);
+static inline __m256i polymullo(__m256i x,__m256i _y);
+static inline __m256i polymullo2(__m256i x,__m256i _y);
+static inline __m256i polymulhi(__m256i x,__m256i _y);
+static inline __m256i polymulhi2(__m256i x,__m256i _y);
+static void montmut_16x16(__m256i * zl, __m256i *zh , __m256i x , __m256i y , 
+		__m256i mask_lo , __m256i mask_hi );
+
+
 
 void print256_num(__m256i var)
 {
@@ -22,6 +33,8 @@ void print256_num(__m256i var)
            val[15], val[14], val[13], val[12], val[11], val[10], val[9], val[8],
            val[7], val[6], val[5], val[4], val[3], val[2], val[1], val[0]);
 }
+
+//TODO: 256bit shift
 
 static inline void print_cmp(__m256i _x,__m256i _y)
 {
@@ -48,7 +61,8 @@ static inline __m256i montproduct(__m256i x,__m256i y)
   e = _mm256_mulhi_epi16(d,v4591_16);
   return _mm256_sub_epi16(hi,e);
 }
-
+//aligr
+//TODO:combine polymullo and polymulhi
 static inline __m256i polymullo(__m256i x,__m256i y)
 {
 	__m256i tmp,z;
@@ -153,6 +167,7 @@ static void montmut_16x16_readable(__m256i * zl, __m256i *zh , __m256i x , __m25
 _mm256_store_si256(zl , z_lo);
 _mm256_store_si256(zh , z_hi);
 }
+
 
 static void montmut_16x16(__m256i * zl, __m256i *zh , __m256i x , __m256i y , 
 		__m256i mask_lo , __m256i mask_hi )
@@ -299,64 +314,19 @@ static void montmut_16x16(__m256i * zl, __m256i *zh , __m256i x , __m256i y ,
 	z_lo=_mm256_permute2x128_si256(z0_lo,z0_hi,0x20);
 	z_hi=_mm256_permute2x128_si256(z0_lo,z0_hi,0x31);
 
-	tmp=_mm256_permute2x128_si256(z1_lo,z1_hi,0x02);
-	tmp2=_mm256_permute2x128_si256(z1_lo,z1_hi,0x13);
+	z0_lo=_mm256_permute2x128_si256(z1_lo,z1_hi,0x02);
+	z0_hi=_mm256_permute2x128_si256(z1_lo,z1_hi,0x13);
 
-	tmp=_mm256_add_epi16(tmp,tmp2);
+	z0_lo=_mm256_add_epi16(z0_lo,z0_hi);
 
-	z0_hi=_mm256_and_si256(tmp,mask_lo);
+	z0_hi=_mm256_and_si256(z0_lo,mask_lo);
 	z_hi=_mm256_add_epi16(z_hi,z0_hi);
 
-	z0_lo=_mm256_and_si256(tmp,mask_hi);
+	z0_lo=_mm256_and_si256(z0_lo,mask_hi);
 	z_lo=_mm256_add_epi16(z_lo,z0_lo);
+
 
 _mm256_store_si256(zl , z_lo);
 _mm256_store_si256(zh , z_hi);
 }
-
-
-#include "benchmark.h"
-
-int main()
-
-{
-	//TODO:reduce register
-	__m256i x,y,z_lo,z_hi;
-	__m256i mask_lo=_mm256_set_epi16(0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-1,-1);
-	__m256i mask_hi=_mm256_set_epi16(-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0,0);
-
-	x=_mm256_set_epi16(1650,2855,279,878,477,2971,1138,4437,2730,1293,3360,3586,4330,219,2514,3189);
-	y=_mm256_set_epi16(3700,3714,378,2596,1606,3397,1877,2113,2571,1332,1284,3924,3663,1608,3361,1427);
-
-	x=montproduct(x,v4158_16);
-	y=montproduct(y,v4158_16);
-
-	// __m256i x2,y2,z_lo2,z_hi2;
-	// x2=_mm256_set_epi16(160,255,79,87,47,291,138,447,230,123,330,386,430,29,254,319);
-	// y2=_mm256_set_epi16(370,371,37,296,106,397,177,213,251,132,124,324,363,168,331,147);
-	struct benchmark bm;
-	char msg[256];
-	bm_init( & bm);
-	for(int i=0;i<10000;i++){
-BENCHMARK( bm , {
-	montmut_16x16( &z_lo , &z_hi , x , y , mask_lo , mask_hi);
-});
-	}
-	bm_dump( msg , 256 , &bm );
-	printf("%s\n", msg );
-
-	z_hi=montproduct(z_hi,v1_16);
-	print256_num(z_hi);
-	z_lo=montproduct(z_lo,v1_16);
-	print256_num(z_lo);
-
-	// print256_num(z_hi2);
-	// print256_num(z_lo2);
-	
-	__m256i z_lo_ans=_mm256_set_epi16(3501,311,4232,3640,830,816,1626,2630,2554,4512,2724,3550,567,2204,151,1022);
-	__m256i z_hi_ans=_mm256_set_epi16(0,3561,3315,1500,1705,1115,4287,116,337,3953,2431,782,548,3224,3051,2230);
-
-	print_cmp(z_hi,z_hi_ans);
-	print_cmp(z_lo,z_lo_ans);
-	return 0;
-}
+#endif
